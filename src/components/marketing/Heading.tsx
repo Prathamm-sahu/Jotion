c4578f689cca726e1c4ec8b4e77ea91afecb3c9f
Pratham Sahu <prathamsahu051@gmail.com>
2023-11-10 19:41:25 +0530

    Added authentication related task

--- src/components/marketing/Heading.tsx
+++ src/components/marketing/Heading.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react'
-import { Button } from '../ui/Button'
+import { Button, buttonVariants } from '../ui/Button'
 import { ArrowRight } from 'lucide-react'
+import { Session } from 'next-auth'
+import { getAuthSession } from '@/lib/auth'
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
 
-interface HeadingProps {
-  
-}
+const Heading = async () => {
+  const session = await getAuthSession()
 
-const Heading: FC<HeadingProps> = ({}) => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Jotion</span></h1>
@@ -14,10 +16,22 @@ const Heading: FC<HeadingProps> = ({}) => {
         Jotion is the connected workspace where <br />
         better, faster work happens.
       </h3>
-      <Button>
-        Enter Jotion
-        <ArrowRight className='h-4 w-4 ml-2' />
-      </Button>
+      {session?.user ? (
+          <Link href="/documents" className={cn(buttonVariants({ variant: "default"}))}>
+            Enter Jotion
+            <ArrowRight className='h-4 w-4 ml-2' />
+          </Link>
+
+      ) : (
+        <>
+          <Link
+            href="/sign-in"
+            className={cn(buttonVariants({ variant: "default" }))}
+          >
+            Sign In
+          </Link>
+        </>
+      )}
     </div>
   )
 }
